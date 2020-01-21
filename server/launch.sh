34ab049c9416881fca269d6c92842f25a756ad7e
Wieland Hagen <wielandhagen@web.de>
2020-01-22 00:51:04 +0100

    improve launch.sh

--- server/launch.sh
+++ server/launch.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-lighttpd -f lighttpd.conf  -D
+#lighttpd -f lighttpd.conf  -D
+
+lighttpd -f lighttpd.conf  -D &
+PID=$!
+
+echo "lighttpd running under PID" $PID
+
+tail -f lighttpd.log
+
+kill $PID
